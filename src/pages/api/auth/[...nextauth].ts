e1aafe6419aba16ec7e4031fa097fda8e8434e67
ya <chinmareno1@gmail.com>
2025-04-29 20:31:56 +0700

    feat: integrate NextAuth with Prisma and credentials provider

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,28 @@
+import prisma from '@/lib/prisma';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials): Promise<any> {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (user && user?.password === credentials.password) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+});
