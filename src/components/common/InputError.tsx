6d0dbb69f972b2ce829cc82bc4b50c7d6cb21e3a
ya <chinmareno1@gmail.com>
2025-05-02 22:25:27 +0700

    init next app router for migration

--- src/components/common/InputError.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const InputError = (message: string | undefined) => {
-  if (!message) return null;
-  return <p className="-mt-2 text-sm text-red-500">{message}</p>;
-};
-
-export default InputError;
