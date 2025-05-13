c712621e122eaa0f95d403fa1f5f782a581c3c17
ya <chinmareno1@gmail.com>
2025-05-14 02:09:01 +0700

    feat(auth): add nextauth to login page
    - login action for validation and db check
    - signIn just for handling session

--- src/actions/auth/loginAction.ts
+++ src/actions/auth/loginAction.ts
@@ -1,42 +1,30 @@
 "use server";
 
-import { redirect } from "next/navigation";
 import { z } from "zod";
 import { loginSchema } from "@/lib/zod";
 import { comparePassword } from "@/lib/bcrypt";
 import { findUserByEmail } from "@/lib/db";
-import { signIn } from "next-auth/react";
 
 type LoginSchema = z.infer<typeof loginSchema>;
 
-export default async function loginAction() {
+export default async function loginAction(data: LoginSchema) {
   try {
-    const response = await signIn("credentials", {
-      email: "adada@asa.co",
-      password: "asasaaa",
-      redirect: false,
-    });
-    return response;
-  } catch (err) {
-    console.log("err.message");
+    loginSchema.parse(data);
+    const { email, password } = data;
+    const user = await findUserByEmail(email);
+    if (!user) {
+      return { success: false, message: "Invalid email or password" };
+    }
+    const isPasswordValid = await comparePassword(password, user.password);
+    if (!isPasswordValid) {
+      return { success: false, message: "Invalid email or password" };
+    }
+    return { success: true, email: user.email };
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return { success: false, message: error.errors[0].message };
+    } else {
+      return { success: false, message: "Login failed" };
+    }
   }
-  // try {
-  //   loginSchema.parse(data);
-  //   const { email, password } = data;
-  //   const user = await findUserByEmail(email);
-  //   if (!user) {
-  //     return { success: false, message: "Invalid email or password" };
-  //   }
-  //   const isPasswordValid = await comparePassword(password, user.password);
-  //   if (!isPasswordValid) {
-  //     return { success: false, message: "Invalid email or password" };
-  //   }
-  // } catch (error) {
-  //   if (error instanceof z.ZodError) {
-  //     return { success: false, message: error.errors[0].message };
-  //   } else {
-  //     return { success: false, message: "Login failed" };
-  //   }
-  // }
-  // redirect("../../home");
 }
