42018b2090bc803315bf1ade05d4ace5f2728eda
ya <chinmareno1@gmail.com>
2025-05-16 02:09:45 +0700

    chore(test): update Jest config for alias support(src) and improve error reporting more cleaner :p

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ export default {
   testEnvironment: "jsdom", // for React DOM APIs in tests
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // for jest-dom
   moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1", //make @ alias work in .test too
     "\\.(css|less|scss|sass)$": "identity-obj-proxy", // handle CSS imports in tests
   },
   transform: {
