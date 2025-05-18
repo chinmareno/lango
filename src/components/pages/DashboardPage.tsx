19fbf6fe0c2a2ff1fa0b0835d1e8bdbf296c2e3e
ya <chinmareno1@gmail.com>
2025-05-18 14:58:47 +0700

    feat(DashboardPage): add JobDetailPanel and make it show the selectedJob from JobListCard

--- src/components/pages/DashboardPage.tsx
+++ src/components/pages/DashboardPage.tsx
@@ -2,10 +2,26 @@
 
 import getMyJobList from "@/actions/job/getJobListAction";
 import { JobListCard } from "../ui/JobListCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { JobDetailPanel } from "../ui/JobDetailPanel";
+import { ISelectedJob } from "@/lib/interfaces/ISelectedJob";
+import { IJobList } from "../../lib/interfaces/IJobList";
 export const DashboardPage = () => {
-  const myJobList = getMyJobList();
-  const [currentDesc, setCurrentDesc] = useState("");
+  useEffect(() => {
+    const myJobList = getMyJobList();
+
+    setJobsList(myJobList);
+  }, []);
+
+  const [jobsList, setJobsList] = useState<IJobList[] | null>(null);
+
+  const [selectedJob, setSelectedJob] = useState<ISelectedJob>({
+    title: "Your Selected Job",
+    type: "Your Job Type",
+    fee: 0,
+    description: "Your Job Description",
+    languagePair: { source: "Specific Language", target: "Specific Language" },
+  });
 
   return (
     <div className="min-h-screen bg-gray-50 p-8">
@@ -20,12 +36,12 @@ export const DashboardPage = () => {
         <section className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <div className="bg-white p-6 rounded-xl shadow-md">
             <h2 className="text-lg font-semibold mb-1">Available Jobs</h2>
-            {myJobList[0] ? (
-              myJobList.map((job) => (
+            {jobsList ? (
+              jobsList.map((job, index) => (
                 <button
-                  key={job.title}
+                  key={index}
                   className="w-full hover:bg-gray-100"
-                  onClick={() => setCurrentDesc(job.description)}
+                  onClick={() => setSelectedJob(job)}
                 >
                   <JobListCard
                     jobTitle={job.title}
@@ -52,6 +68,9 @@ export const DashboardPage = () => {
               Track your payments and job history.
             </p>
           </div>
+        </section>
+        <section className="flex flex-row justify-center bg-amber-300 mt-10">
+          <JobDetailPanel {...selectedJob} />
         </section>
       </main>
     </div>
