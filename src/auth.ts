5db3ed3d55fef71efe354f8a36e5e04a70d501f8
ya <chinmareno1@gmail.com>
2025-05-24 02:41:43 +0700

    feat: add clientId and translatorId if available to jwt

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
-import { findUserByEmail } from "@/lib/db/findUserByEmail";
 import Credentials from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
+import { findUserByEmail } from "./lib/db/user";
 
 export const {
   handlers: { GET, POST },
@@ -21,7 +21,20 @@ export const {
           if (!credentials) {
             return null;
           }
-          const user = await findUserByEmail(credentials.email as string);
+          const userData = await findUserByEmail(credentials.email as string);
+          const user = {
+            userId: userData?.id,
+            email: userData?.email,
+            currentRole: userData?.currentRole,
+            clientProfileId: "",
+            translatorProfileId: "",
+          };
+          if (userData?.translatorProfile) {
+            user.translatorProfileId = userData.translatorProfile.id;
+          }
+          if (userData?.clientProfile) {
+            user.clientProfileId = userData.clientProfile.id;
+          }
           return user;
         } catch (error) {
           return null;
@@ -33,17 +46,30 @@ export const {
   callbacks: {
     jwt: async ({ token, user }) => {
       if (user) {
+        token.id = user.userId;
         token.email = user.email;
         token.name = user.name;
         token.currentRole = user.currentRole;
+        if (user.translatorId) {
+          token.translatorId = user.translatorId;
+        }
+        if (user.clientId) {
+          token.clientRoleId = user.clientId;
+        }
       }
       return token;
     },
     session: async ({ session, token }) => {
-      if (token.id && token.email) {
+      if (token.id && token.email && token.currentRole) {
+        session.user.id = token.id as string;
         session.user.email = token.email;
-        session.user.name = token.name;
         session.user.currentRole = token.currentRole as string;
+      }
+      if (token.translatorId) {
+        session.user.translatorId = token.translatorId as string;
+      }
+      if (token.clientId) {
+        session.user.clientId = token.clientId as string;
       }
       return session;
     },
