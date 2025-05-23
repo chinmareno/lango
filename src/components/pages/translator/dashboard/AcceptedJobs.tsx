85c3f952e789585ee170672a3e00f08c1c735c05
ya <chinmareno1@gmail.com>
2025-05-24 02:46:47 +0700

    feat: add AcceptedJobs for showing accepted translator's applications

--- src/components/pages/translator/dashboard/AcceptedJobs.tsx
+++ src/components/pages/translator/dashboard/AcceptedJobs.tsx
@@ -1,46 +1,54 @@
 import { JobListCard } from "@/components/ui/JobListCard";
-import { Job } from "@/lib/interfaces/Job";
 import { Dispatch, SetStateAction, useMemo } from "react";
-import { SelectedJobProps } from ".";
+import { IApplicationWithJob } from "@/lib/interfaces/IApplication";
 
 interface IAcceptedJobs {
-  jobsList: Job[] | null;
-  setSelectedJob: Dispatch<SetStateAction<SelectedJobProps | null>>;
+  applications: IApplicationWithJob[] | null;
+  setSelectedApplication: Dispatch<SetStateAction<IApplicationWithJob | null>>;
 }
 
-export const AcceptedJobs = ({ jobsList, setSelectedJob }: IAcceptedJobs) => {
-  if (jobsList === null) {
+export const AcceptedJobs = ({
+  applications,
+  setSelectedApplication,
+}: IAcceptedJobs) => {
+  if (applications === null) {
     return (
       <div className="bg-white p-6 rounded-xl shadow-md">
+        <h2 className="text-lg font-semibold mb-1">No Accepted Projects</h2>
         <p className="text-gray-700 text-sm">
           Continue working on projects you’re assigned to.
         </p>
       </div>
     );
   }
-  const userId = "reno";
-  const acceptedJobsList = useMemo(
+  const acceptedApplications = useMemo(
     () =>
-      jobsList.filter(
-        (job) => job.takenBy === userId && job.status === "taken"
-      ),
-    [jobsList]
+      applications
+        .filter((application) => application.status === "ACCEPTED")
+        .sort((a) => {
+          if (a.job.status === "IN_PROGRESS") return -1;
+          else if (a.job.status === "OPEN") return 1;
+          return 0;
+        }),
+    [applications]
   );
   return (
     <div className="bg-white p-6 rounded-xl shadow-md">
       <h2 className="text-lg font-semibold mb-1">Accepted Projects</h2>
-      {acceptedJobsList.map((job, index) => (
+      {acceptedApplications.map((application, index) => (
         <button
           key={index}
           className="w-full hover:bg-gray-100"
           onClick={() =>
-            setSelectedJob({ ...job, selectedJobType: "onprogress jobs" })
+            setSelectedApplication({
+              ...application,
+            })
           }
         >
           <JobListCard
-            jobTitle={job.title}
-            jobType={job.type}
-            jobFee={job.fee}
+            jobTitle={application.job.title}
+            jobType={application.job.paymentType}
+            jobFee={application.job.fee}
           />
         </button>
       ))}
