9d561460eb4879887ffc980fe38152276f09d43b
ya <chinmareno1@gmail.com>
2025-05-24 02:49:02 +0700

    refactor: update JobDetailPanel to match new data structure with Job and Application separated

--- src/components/ui/JobDetailPanel.tsx
+++ src/components/ui/JobDetailPanel.tsx
@@ -1,51 +1,65 @@
-import { Job } from "@/lib/interfaces/Job";
-import { SelectedJobProps } from "../pages/translator/dashboard";
+import { IApplicationWithJob } from "@/lib/interfaces/IApplication";
+
+interface JobDetailPanelProps {
+  selectedApplication: IApplicationWithJob | null;
+}
 
 export const JobDetailPanel = ({
-  title,
-  type,
-  fee,
-  description,
-  languagePair,
-  status,
-  selectedJobType,
-}: SelectedJobProps) => {
+  selectedApplication,
+}: JobDetailPanelProps) => {
+  if (selectedApplication === null) {
+    return (
+      <div className="bg-white p-6 rounded-xl shadow-md w-full max-w-2xl text-center">
+        <h2 className="text-xl font-semibold text-gray-800 mb-2">
+          No Job Selected
+        </h2>
+        <p className="text-gray-600 text-sm">
+          Click on a job from the list to view its details here.
+        </p>
+      </div>
+    );
+  }
+  const { job } = selectedApplication;
   return (
     <div
       className={`relative bg-white rounded-xl shadow-md p-6 mt-8 ${
-        type === "hourly" ? "border-blue-300" : "border-green-300"
+        job.paymentType === "FIXED" ? "border-blue-300" : "border-green-300"
       } border-4`}
     >
-      {status === "taken" && (
-        <div className="absolute top-2 right-2 bg-yellow-400 text-white text-xs px-2 py-1 rounded-md font-bold">
-          Taken
-        </div>
-      )}
-
-      <h2 className="text-xl font-semibold mb-2 text-gray-900">{title}</h2>
-
+      <div className="absolute top-2 right-2 bg-yellow-400 text-white text-xs px-2 py-1 rounded-md font-bold">
+        {selectedApplication.status === "REJECTED" && "Rejected"}
+        {selectedApplication.status === "PENDING" && "PENDING"}
+        {selectedApplication.status === "ACCEPTED" && "ACCEPTED"}
+      </div>
+      <h2 className="text-xl font-semibold mb-2 text-gray-900">{job.title}</h2>
       <div className="text-sm text-gray-600 mb-1">
-        Type: <span className="font-medium capitalize">{type}</span>
+        Type:
+        <span className="font-medium capitalize">{job.paymentType}</span>
       </div>
       <div className="text-sm text-gray-600 mb-1">
         Fee:
         <span className="font-medium">
-          {fee}
-          {type === "hourly" ? "$/hour" : "$/project"}
+          {job.fee}
+          {job.paymentType === "HOURLY" ? "$/hour" : "$/project"}
         </span>
       </div>
       <div className="text-sm text-gray-600 mb-4">
-        Language:{" "}
+        Language:
         <span className="font-medium">
-          {languagePair.source} ➜ {languagePair.target}
+          {job.sourceLanguage} ➜ {job.targetLanguage}
         </span>
       </div>
-
       <p className="text-gray-800 text-sm leading-relaxed whitespace-pre-line mb-6">
-        {description}
+        {job.description}
       </p>
-
-      {selectedJobType === "available jobs" ? (
+      {selectedApplication.status === "PENDING" && job.status === "OPEN" && (
+        <div className="flex gap-3">
+          <button className="px-3 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 flex-shrink-0">
+            Delete Job
+          </button>
+        </div>
+      )}
+      {selectedApplication.status === "ACCEPTED" && job.status === "OPEN" && (
         <div className="flex gap-3">
           <button className="px-3 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 flex-shrink-0">
             Chat Client
@@ -57,25 +71,32 @@ export const JobDetailPanel = ({
             Delete Job
           </button>
         </div>
-      ) : selectedJobType === "onprogress jobs" ? (
-        <div className="flex gap-3">
-          <button className="px-3 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 flex-shrink-0">
-            Chat Client
-          </button>
-          <button className="flex-grow px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 font-semibold">
-            Finish Job
-          </button>
-          <button className="px-3 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 flex-shrink-0">
-            Cancel Job
-          </button>
-        </div>
-      ) : (
-        selectedJobType === "done jobs" && (
-          <button className="px-3 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 flex-shrink-0">
-            Chat Client
-          </button>
-        )
       )}
+      {selectedApplication.status === "ACCEPTED" &&
+        job.status === "IN_PROGRESS" && (
+          <div className="flex gap-3">
+            <button className="px-3 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 flex-shrink-0">
+              Chat Client
+            </button>
+            <button className="flex-grow px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 font-semibold">
+              Finish Job
+            </button>
+            <button className="px-3 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 flex-shrink-0">
+              Cancel Job
+            </button>
+          </div>
+        )}
+      {selectedApplication.status === "ACCEPTED" &&
+        job.status === "COMPLETED" && (
+          <div className="flex gap-3">
+            <button className="px-3 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 flex-shrink-0">
+              Chat Client
+            </button>
+            <button className="flex-grow px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 font-semibold">
+              Rate Job
+            </button>
+          </div>
+        )}
     </div>
   );
 };
